646a3c6a05e4d08d7b03bc3b81a5d60055ebaf18
toharsimchi <toharya.simchi@mail.huji.ac.il>
2024-01-24 09:43:14 +0200

    efficient solution

--- Array/MinCostClimbingStairs.py
+++ Array/MinCostClimbingStairs.py
@@ -1,6 +1,6 @@
 class Solution(object):
 
-    # non efficient solution O(2^n): recursive tree in height of n which splits to 2 in each stage
+# non efficient solution O(2^n): recursive tree in height of n which splits to 2 in each stage
     # def minCostClimbingStairs(self, cost):
     #     """
     #     :type cost: List[int]
@@ -15,3 +15,20 @@ class Solution(object):
     #         )
         
     #     return min(_helper(len(cost)-1), _helper(len(cost)-2))
+
+
+# an efficient solution O(n): based on DP approach, table 1Xn each cell computable in O(1)
+    def minCostClimbingStairs(self, cost):
+    #     """
+    #     :type cost: List[int]
+    #     :rtype: int
+    #     """
+        dp = [0]*len(cost)
+        n = len(cost)-1
+        dp[0] = cost[n]
+        dp[1] = cost[n-1]
+
+        for i in range(2,len(cost)):
+            dp[i] = cost[n-i] + min( dp[i-1] , dp[i-2])
+
+        return min( dp[-1], dp[-2]) 
